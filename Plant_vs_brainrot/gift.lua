a53dd4f4c78255c638d2262b9ddd8178e7866757
Duyy <hoangduyanh909@gmail.com>
2025-10-02 23:39:14 +0700

    Update gift.lua

--- Plant_vs_brainrot/gift.lua
+++ Plant_vs_brainrot/gift.lua
@@ -1,3 +1,7 @@
+if not game:IsLoaded() then
+    repeat game.Loaded:Wait() until game:IsLoaded()
+end
+wait(5)
 local listcangift = {"Dragon Cannelloni"} -- tên Item cần gửi
 local listnguoicangift = { "MrsAndrewkn0ll2005",
     "Emmajolt2012",
