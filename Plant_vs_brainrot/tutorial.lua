bd9ccb7529751801b83102bd9305d1bd1b94ff6b
Duyy <hoangduyanh909@gmail.com>
2025-10-03 01:00:22 +0700

    Update tutorial.lua

--- Plant_vs_brainrot/tutorial.lua
+++ Plant_vs_brainrot/tutorial.lua
@@ -1,4 +1,7 @@
-if game:GetService("Players").LocalPlayer.PlayerGui.HUD:FindFirstChild('Tutorial') and game:GetService("Players").LocalPlayer.PlayerGui.HUD.Tutorial.Visible == true then
-    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpawnTutorialBrainrot"):FireServer()
-    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("FinishTutorial"):FireServer()
-end
+while true do
+    if game:GetService("Players").LocalPlayer.PlayerGui.HUD:FindFirstChild('Tutorial') and game:GetService("Players").LocalPlayer.PlayerGui.HUD.Tutorial.Visible == true then
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpawnTutorialBrainrot"):FireServer()
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("FinishTutorial"):FireServer()
+    end
+    wait(5)
+end    
