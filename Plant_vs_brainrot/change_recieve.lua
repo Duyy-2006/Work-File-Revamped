50abe29dcda8073bd9e45527ded4248fefa012f9
Duyy <hoangduyanh909@gmail.com>
2025-10-05 22:54:31 +0700

    Update change_recieve.lua

--- Plant_vs_brainrot/change_recieve.lua
+++ Plant_vs_brainrot/change_recieve.lua
@@ -2,19 +2,20 @@ if not game:IsLoaded() then
     repeat game.Loaded:Wait() until game:IsLoaded()
 end
 local TreeTable = {
-    "Dragon Cannelloni",
-    "Bredda Ratto",
-    "Gattolini Owlini",
-    "Squalo Cavallo",
-    "Arminini Bodybuilderini",
-    "Blueberrinni Octopussini",
-    "Brri Brri Bicus Dicus Bombicus",
-    "Crazylone Pizaione",
-    "Garamararam",
-    "Vacca Saturno Saturnita",
-    "Los Tralaleritos",
-    "Pot Hotspot",
-    "Los Mr Carrotitos",
+    "La Tomatoro",
+        'Dragon Cannelloni',
+        'Bredda Ratto',
+        'Gattolini Owlini',
+        'Squalo Cavallo',
+        'Arminini Bodybuilderini',
+        'Blueberrinni Octopussini',
+        'Brri Brri Bicus Dicus Bombicus',
+        'Crazylone Pizaione',
+        'Garamararam',
+        'Vacca Saturno Saturnita',
+        'Los Tralaleritos',
+        'Pot Hotspot',
+        'Los Mr Carrotitos',
 }
 
 
