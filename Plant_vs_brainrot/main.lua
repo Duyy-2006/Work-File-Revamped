73cb698433793532df7d3aceaa690b8998026c77
Duyy <hoangduyanh909@gmail.com>
2025-10-09 00:47:30 +0700

    Update main.lua

--- Plant_vs_brainrot/main.lua
+++ Plant_vs_brainrot/main.lua
@@ -2,7 +2,7 @@
 if not game:IsLoaded() then
     repeat game.Loaded:Wait() until game:IsLoaded()
 end
-local type = "Recieve"
+local type = "Gify"
 local TeleportService = game:GetService("TeleportService")
 local PlaceId         = game.PlaceId
 local JobId           = game.JobId
