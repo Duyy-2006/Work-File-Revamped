de233dd5403f42fae275019f841b43ee93339056
Duyy <hoangduyanh909@gmail.com>
2025-10-22 22:34:39 +0700

    Update change_recieve.lua

--- Plant_vs_brainrot/change_recieve.lua
+++ Plant_vs_brainrot/change_recieve.lua
@@ -1,9 +1,7 @@
 if not game:IsLoaded() then
     repeat game.Loaded:Wait() until game:IsLoaded()
 end
-local TreeTable = {
-    "Tomatrio",
-}
+local TreeTable = { "Tomatrio", "Mango", "Mr Carrot", "Shroombino" } 
 
 
 
