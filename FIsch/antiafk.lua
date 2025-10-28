f72748078a0bcf2d002065fca2caf793ac286d3e
Duyy <hoangduyanh909@gmail.com>
2025-10-29 00:12:46 +0700

    Create antiafk.lua

---
+++ FIsch/antiafk.lua
@@ -0,0 +1,19 @@
+if not game:IsLoaded() then
+    repeat game.Loaded:Wait() until game:IsLoaded()
+end
+
+task.wait(10)
+
+local player = game:GetService("Players").LocalPlayer
+local leaderstats = player:WaitForChild("leaderstats")
+local level = leaderstats:WaitForChild("Level")
+print(level.Value)
+
+while true do
+    local first = level.Value
+    task.wait(300)
+    local second = level.Value
+    if first ~= second then
+        player:Kick("Error")
+    end 
+end
