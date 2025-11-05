5a2d40b34fbd4a5e1d79c061414875ed1f76adcd
Duyy <anhhoangduy659@gmail.com>
2025-11-05 20:28:52 +0700

    Create Anime Vanguard

---
+++ Anime Vanguard/Mainscript.lua
@@ -0,0 +1,33 @@
+if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
+wait(3)
+setfpscap(10)
+_G.VanguardSettings = {
+    ['ClaimDaily'] = true,
+    ['ClaimQuests'] = true,
+    ['ClaimAchievement'] = true,
+    ['ClaimBattlepass'] = true,
+    ['ClaimMileStones'] = true,
+    ['RedeemCodes'] = true,
+    ['JoinGames'] = true,
+    ['Summon'] = {
+        ['LockUnits'] = false,
+        ['Name'] = {''}
+    },
+    ['Webhooks'] = {
+        ['Enabled'] = true,
+        ['Url'] = ''
+    },
+    ["TeaFarm"] = {
+        ["Enabled"] = true,
+        ["FarmAtLevel"] = 120,
+        ["RandomUnits"] = {
+            ["SellUnitsNotLock"] = true,
+            ["RandomWhenHaveTea"] = 100000,
+            ["Lock"] = {
+                "Iscanur (Pride)"
+            }
+        }
+    }
+}
+script_key="MWRzkqkBFehZPykSXInTXiIeQkEgyIzY";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/6756a57cd51293c409a1f7627cba5572.lua"))()
