0e8e4e76fdebad272900aca24691f8959ae95bed
Duyy <anhhoangduy659@gmail.com>
2025-11-05 22:46:54 +0700

    Create mm2

---
+++ MM2/mainscript.lua
@@ -0,0 +1,17 @@
+script_key = "eeLluisnaQwCWznUGhHiZjNjnktVHIAp"
+getgenv().TLfig = {
+    AutoFarm  = true,
+    Modefarm  = "",
+    FpsLock   = {true, 10},
+    Webhook   = {
+        Enabled = true,
+        URL = "",
+        Rarity = { Common = false, Uncommon = false, Rare = true, Legendary = true, Godly = true }
+    }
+}
+repeat
+    local success, err = pcall(function() 
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7c742b62fa7b1eafb8b5e8b25a07f0ef.lua"))()
+        task.wait(20)
+    end)
+until success
