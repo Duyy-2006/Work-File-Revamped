c891dd2dd6eed967f9b1775b7bb183ae25638c03
Duyy <hoangduyanh909@gmail.com>
2025-11-06 15:02:27 +0700

    Update trackstats.lua

--- Fish It/trackstats.lua
+++ Fish It/trackstats.lua
@@ -1,1 +1,3 @@
+if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
+wait(5)
 _G.Config={UserID="a7916749-0080-4d07-b4aa-3c4cb61484a1",discord_id="935143471243726858",Note="Pc"}local s;for i=1,5 do s=pcall(function()loadstring(game:HttpGet("https://cdn.yummydata.click/scripts/fishit"))()end)if s then break end wait(5)end
