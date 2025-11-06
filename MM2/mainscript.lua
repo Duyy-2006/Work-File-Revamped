ddc87894b437a4d231ec7f1f728df9c0b3c42574
Duyy <hoangduyanh909@gmail.com>
2025-11-06 14:50:23 +0700

    Update mainscript.lua

--- MM2/mainscript.lua
+++ MM2/mainscript.lua
@@ -1,3 +1,7 @@
+if not game:IsLoaded() then
+    repeat game.Loaded:Wait() until game:IsLoaded()
+end
+wait(5)
 script_key="HFPAUALdlgXSXRlcDYGlHtpXHAJePzvS";
 getgenv().Config = {
     HUDHopMinGain = 60     -- Candies                    -- Minimum candy collected (supports "HUDHopWindowSec") 
