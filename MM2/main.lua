54690a532150ec3317091c4e8393e332842ed933
Duyy <hoangduyanh909@gmail.com>
2025-11-06 16:02:27 +0700

    Update main.lua

--- MM2/main.lua
+++ MM2/main.lua
@@ -12,7 +12,7 @@ task.spawn(function()
         HUDHopMinGain   = 60     -- Candies                    -- Minimum candy collected (supports "HUDHopWindowSec") 
         HUDHopWindowSec = 600    -- Second                     -- Minimum farming time (supports "HUDHopMinGain") 
         FPSCap          = 5,                                  -- Best fps optimized, can change if you want! (recommend 5 fps+)
-        Mode            = "",                                 -- "battlepass" or "crate"
+        Mode            = "battlepass",                                 -- "battlepass" or "crate"
         OpenDelay       = 2.5,    -- Second                    -- Crate Open Delay
         Webhook         = "your webhook here",                -- Crate notify
         DiscordID       = "put your discord id here"          -- Crate notify (need metion)
@@ -21,3 +21,4 @@ task.spawn(function()
     -- USE THE CORRECT LOADSTRING FOR YOUR NUMBER OF TABS! PICK ONLY ONE LOADSTRING!
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/fbdaaeddcf4c27445764a37ba601f35a.lua"))() -- 120 TABS
 end)
+
