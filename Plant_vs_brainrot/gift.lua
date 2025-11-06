24c24bf0221ac733d97c26e076a527a228e2508a
Duyy <hoangduyanh909@gmail.com>
2025-11-07 00:01:33 +0700

    Update gift.lua

--- Plant_vs_brainrot/gift.lua
+++ Plant_vs_brainrot/gift.lua
@@ -5,7 +5,7 @@ end
 task.wait(5)
 
 -- // Config
-local listcangift = {"King Limone",
+local listcangift = {"King Limone", "Tomatrio", "Shroombino", "Mango", 
 }
 local listnguoicangift = {
     "minny_nxy","Hug0Hannah47","MrsAndrewkn0ll2005","Emmajolt2012","MrsGamerKnack2006",
