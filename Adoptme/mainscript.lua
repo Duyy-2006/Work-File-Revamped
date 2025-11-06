17ff993c4168040c460b6b1a3476a5187ba85422
Duyy <hoangduyanh909@gmail.com>
2025-11-06 14:58:58 +0700

    Update mainscript.lua

--- Adoptme/mainscript.lua
+++ Adoptme/mainscript.lua
@@ -2,23 +2,36 @@ if not game:IsLoaded() then
     repeat game.Loaded:Wait() until game:IsLoaded()
 end
 wait(5)
-setfpscap(2)
-script_key="dCsrDWdsslpaMkVGHZmrKbKOdfPkMXCG";
-getgenv().Config = {
-    ["PetAMountToBuy"] = "0", -- can be 999
-    ["WhatCategory"] = "pets", -- gifts if WhatCategory = "" then = pets
-    ["PetRemoteToBuy"] = "aztec_egg_2025_aztec_egg", -- exemple for gifts halloween_2025_spider_box
+task.spawn(function()
+    script_key="dCsrDWdsslpaMkVGHZmrKbKOdfPkMXCG";
+    getgenv().Config = {
+        ["PetAMountToBuy"] = "0", -- can be 999
+        ["WhatCategory"] = "pets", -- gifts if WhatCategory = "" then = pets
+        ["PetRemoteToBuy"] = "retired_egg", -- exemple for gifts halloween_2025_spider_box
 
-    ["PetFarmAutoSwitchFullGrown"] = false,
-    ["PetFarmActive"] = false,
-    ["LitePetFarmActive"] = true,
-    ["EggFarmActive"] = false,
-    
-    ["HideUselessGui"] = true,
-    ["Blur_username"] = true,
-    ["Blazing_Lion_Log"] = false,
-    ["DiscordId"] = "123456",
-    ["Webhook"] = "",
-    ["LegendaryWebhook"] = "",
-}
+        ["PetFarmAutoSwitchFullGrown"] = false,
+        ["PetFarmActive"] = false, -- farm potion
+        ["EggFarmActive"] = true, -- farm egg, if no more eggs will farm potion
+
+        ["NeonMegaFarm"] = false,
+        ["BuyEgg"] = "retired_egg", -- only work with NeonMegaFarm, will loop buy this egg when enough bucks
+        
+        ["AutoReleasePet"] = false,
+        ["RecycleWebhook"] = "",
+        ["common"] = false,
+        ["uncommon"] = false,
+        ["rare"] = false,
+        ["ultra_rare"] = false,
+
+        ["HideUselessGui"] = true,
+        ["Blur_username"] = true,
+        ["Blazing_Lion_Log"] = false,
+        ["DiscordId"] = "123456",
+        ["Webhook"] = "",
+        ["LegendaryWebhook"] = "",
+        ["NeonMegaWebhook"] = "",
+    }
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
+end)
+setfpscap(2)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/66567bfd337b57eb059b58dbe1badb89.lua"))()
