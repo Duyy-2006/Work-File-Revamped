2cdec6b69ce93958fd7e6f9f0e338449bf3548f2
Duyy <hoangduyanh909@gmail.com>
2025-11-09 17:25:47 +0700

    Update change_recieve.lua

--- Plant_vs_brainrot/change_recieve.lua
+++ Plant_vs_brainrot/change_recieve.lua
@@ -41,7 +41,7 @@ while true do
         end
     end
 
-    if matchCount > 0 then
+    if matchCount > 2 then
         writefile(filename, "Yummytool")
         player:Kick("du pet r be iu di ra th")
     else 
