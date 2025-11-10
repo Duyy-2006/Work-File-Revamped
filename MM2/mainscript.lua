b8b03b1d6e20345ae08a15cf211d38597595026e
Duyy <hoangduyanh909@gmail.com>
2025-11-11 02:44:16 +0700

    Update mainscript.lua

--- MM2/mainscript.lua
+++ MM2/mainscript.lua
@@ -3,21 +3,21 @@ if not game:IsLoaded() then
 end
 
 wait(5)
-setfpscap(5)
 
-task.spawn(function()
-    script_key = "BVpLUEgYVUXtqprzrfKgxVRTqrUtAlnm";
-
-    getgenv().Config = {
-        HUDHopMinGain   = 500,     -- Candies                    -- Minimum candy collected (supports "HUDHopWindowSec") 
-        HUDHopWindowSec = 3600,    -- Second                     -- Minimum farming time (supports "HUDHopMinGain") 
-        FPSCap          = 5,                                  -- Best fps optimized, can change if you want! (recommend 5 fps+)
-        Mode            = "",                                 -- "battlepass" or "crate"
-        OpenDelay       = 2.5,    -- Second                    -- Crate Open Delay
-        Webhook         = "your webhook here",                -- Crate notify
-        DiscordID       = "put your discord id here"          -- Crate notify (need metion)
+script_key = "eeLluisnaQwCWznUGhHiZjNjnktVHIAp"
+getgenv().TLfig = {
+    AutoFarm  = true,
+    Modefarm  = "",
+    FpsLock   = { true, 10},
+    Webhook   = {
+        Enabled = true,
+        URL = "",
+        Rarity = { Common = false, Uncommon = false, Rare = true, Legendary = true, Godly = true }
     }
-
-    -- USE THE CORRECT LOADSTRING FOR YOUR NUMBER OF TABS! PICK ONLY ONE LOADSTRING!
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/415a39cc0109e5b58e77746da4f782d4.lua"))() -- 300 TABS
-end)
+}
+repeat
+    local success, err = pcall(function() 
+        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7c742b62fa7b1eafb8b5e8b25a07f0ef.lua"))()
+        task.wait(20)
+    end)
+until success
