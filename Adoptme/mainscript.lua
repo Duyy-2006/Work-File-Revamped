e333ab284e1c173bbb0e21a3dcdd64069245ec9f
Duyy <hoangduyanh909@gmail.com>
2025-12-08 20:44:17 +0700

    Update mainscript.lua

--- Adoptme/mainscript.lua
+++ Adoptme/mainscript.lua
@@ -7,7 +7,7 @@ task.spawn(function()
     script_key="dCsrDWdsslpaMkVGHZmrKbKOdfPkMXCG";
     getgenv().Config = {
         ["PetAMountToBuy"] = "0", -- can be 999
-        ["WhatCategory"] = "pets", -- gifts if WhatCategory = "" then = pets
+        ["WhatCategory"] = "", -- gifts if WhatCategory = "" then = pets
         ["PetRemoteToBuy"] = "aztec_egg_2025_aztec_egg", -- exemple for gifts halloween_2025_spider_box
 
 
@@ -17,11 +17,11 @@ task.spawn(function()
 
         ["EggFarmActive"] = false, -- farm egg, if no more eggs will farm potion, if found new egg will farm them to
         ["EggToIgnore"] = {""}, -- eggs to ignore when eggfarm active
-        ["LoopBuyEgg"] = true, -- when enough bucks will buy egg, only work with eggfarm
+        ["LoopBuyEgg"] = false, -- when enough bucks will buy egg, only work with eggfarm
         ["EggToBuyEgg"] = "aztec_egg_2025_aztec_egg",
 
 
-        ["NeonMegaFarm"] = true,
+        ["NeonMegaFarm"] = false,
         ["BuyEgg"] = "aztec_egg_2025_aztec_egg", -- only work with NeonMegaFarm, will loop buy this egg when enough bucks
         
 
