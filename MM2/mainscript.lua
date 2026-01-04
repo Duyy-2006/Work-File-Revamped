4ef2d27f12c7581c1dcb30f2e4d1db052e824140
Duyy <anhhoangduy659@gmail.com>
2026-01-04 18:40:43 +0700

    Update mainscript.lua

--- MM2/mainscript.lua
+++ MM2/mainscript.lua
@@ -1,23 +1,11 @@
-if not game:IsLoaded() then
-    repeat game.Loaded:Wait() until game:IsLoaded()
-end
-
-wait(5)
-
-script_key = "eeLluisnaQwCWznUGhHiZjNjnktVHIAp"
-getgenv().TLfig = {
-    AutoFarm  = true,
-    Modefarm  = "",
-    FpsLock   = { true, 10},
-    Webhook   = {
-        Enabled = true,
-        URL = "",
-        Rarity = { Common = false, Uncommon = false, Rare = true, Legendary = true, Godly = true }
-    }
+script_key="yIftqDhcUjivCHqxnGQrcOGClbMYHOCW";
+getgenv().Config = {
+    HUDHopMinGain = 80,     -- Tokens                    -- Minimum snow tokens collected (in 10 minutes)
+    HUDHopWindowSec = 600,  -- Second                    --
+    FPSCap     = 5,                                      -- Best fps optimized, can change if you want! (recommend 5 fps+)
+    AutoSendGiftSelf = false,                             -- true or false
+    Mode       = "mysterybox",                     -- "battlepass" or "crate" or "mysterybox"
+    Webhook    = "your_webhook_here",                    -- Crate notify
+    DiscordID  = "put_your_discord_id_here"              -- Crate notify (need metion)
 }
-repeat
-    local success, err = pcall(function() 
-        loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/7c742b62fa7b1eafb8b5e8b25a07f0ef.lua"))()
-        task.wait(20)
-    end)
-until success
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/415a39cc0109e5b58e77746da4f782d4.lua"))() -- 300 TABS
