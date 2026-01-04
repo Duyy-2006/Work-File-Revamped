323f155927f914859c1ca867b70ec940ad544bd7
Duyy <anhhoangduy659@gmail.com>
2026-01-04 21:38:55 +0700

    Update mainscript.lua

--- MM2 Gift/mainscript.lua
+++ MM2 Gift/mainscript.lua
@@ -3,9 +3,9 @@ getgenv().Config = {
     HUDHopMinGain = 80,     -- Tokens                    -- Minimum snow tokens collected (in 10 minutes)
     HUDHopWindowSec = 600,  -- Second                    --
     FPSCap     = 5,                                      -- Best fps optimized, can change if you want! (recommend 5 fps+)
-    AutoSendGiftSelf = false,                             -- true or false
+    AutoSendGiftSelf = true,                             -- true or false
     Mode       = "mysterybox",                     -- "battlepass" or "crate" or "mysterybox"
-    Webhook    = "your_webhook_here",                    -- Crate notify
-    DiscordID  = "put_your_discord_id_here"              -- Crate notify (need metion)
+    Webhook    = "https://discord.com/api/webhooks/967820762734030899/lPujX41Md70Per8IZyU6jwMFHpvl-_5MFqQSzguUMGYZep1a_t0HHYzTFTJ8gPBolGrP",                    -- Crate notify
+    DiscordID  = "935143471243726858"              -- Crate notify (need metion)
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/415a39cc0109e5b58e77746da4f782d4.lua"))() -- 300 TABS
