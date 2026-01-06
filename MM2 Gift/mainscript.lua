7c713c2fe1727856238f340e0b312620d1f48092
Duyy <anhhoangduy659@gmail.com>
2026-01-06 20:10:26 +0700

    Update mainscript.lua

--- MM2 Gift/mainscript.lua
+++ MM2 Gift/mainscript.lua
@@ -4,7 +4,7 @@ getgenv().Config = {
     HUDHopWindowSec = 600,  -- Second                    --
     FPSCap     = 5,                                      -- Best fps optimized, can change if you want! (recommend 5 fps+)
     AutoSendGiftSelf = true,                             -- true or false
-    Mode       = "mysterybox",                     -- "battlepass" or "crate" or "mysterybox"
+    Mode       = "crate",                     -- "battlepass" or "crate" or "mysterybox"
     Webhook    = "https://discord.com/api/webhooks/967820762734030899/lPujX41Md70Per8IZyU6jwMFHpvl-_5MFqQSzguUMGYZep1a_t0HHYzTFTJ8gPBolGrP",                    -- Crate notify
     DiscordID  = "935143471243726858"              -- Crate notify (need metion)
 }
