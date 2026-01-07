636601cb4ac16da1d564de5bab2f993a9864edd5
Duyy <anhhoangduy659@gmail.com>
2026-01-07 21:43:33 +0700

    a

---
+++ GPO/mainscript.lua
@@ -0,0 +1,36 @@
+script_key="4eb7a8e774431e01e423ed86a849e752"
+getgenv().YuukiHub = {
+    ["Level Cap"] = 675,
+    ["Stats Cap"] = {
+        ["Strength"] = 1000,
+        ["Stamina"] = 0,
+        ["Defense"] = 0,
+        ["GunMastery"] = 0,
+        ["SwordMastery"] = 0,
+    },
+    ["Private Code"] = {
+        ["Amb3rAl3xandra"] = "p7gnANYSll",
+        ["Brianna_Calvin2021"] = "p7gnANYSll",
+        ["ShioriDeeChloe"] = "p7gnANYSll",
+        ["Gabri3lBrianna2020"] = "p7gnANYSll",
+        ["BlairGertrudeAmy"] = "p7gnANYSll",
+        ["Fiona_Gail2016"] = "p7gnANYSll",
+        ["Cam3r0n_Chl032005"] = "p7gnANYSll",
+        ["MingIsabelAngela"] = "p7gnANYSll",
+        ["KaoriHopeAmy2016"] = "p7gnANYSll",
+        ["HowardDaltonDeclan20"] = "p7gnANYSll",
+        ["DannyIsaacGavin"] = "p7gnANYSll",
+        ["FrancisDebra2002"] = "p7gnANYSll",
+        ["AudreyBarbaraAndrea2"] = "p7gnANYSll",
+        ["Fumiya_Clara2011"] = "p7gnANYSll",
+        ["XiaEdith2023"] = "p7gnANYSll",
+        ["DariusAmeliaGraham20"] = "p7gnANYSll",
+        ["LinDeclanGrant2016"] = "p7gnANYSll",
+        ["Qing_Holly2019"] = "p7gnANYSll",
+        ["DorothyDaphne11"] = "p7gnANYSll",
+        ["DerekDaphne43"] = "p7gnANYSll",
+    },
+    ["Performance"] = {["FPS Lock"] = 10, ["Low CPU"] = true}
+}
+
+loadstring(game:HttpGet('https://yuukihubz.net/scripts/c1f2b6d1b23fd77a53b40f271ef43e3a'))()
