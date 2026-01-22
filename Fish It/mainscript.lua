4ca7a0bee8db4923731bff71e3078aff26145c5d
Duyy <anhhoangduy659@gmail.com>
2026-01-22 14:59:25 +0700

    Update mainscript.lua

--- Fish It/mainscript.lua
+++ Fish It/mainscript.lua
@@ -1,16 +1,54 @@
-if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
-
-_G.Setting =  {
-    Events = {
-        Join = false,
-        IgnoredList = {
-            "Ghost Shark Hunt"
-        }
+script_key="EXEikEPXJKNBRMldzWtzaJIxJmwIjbcY";
+setfpscap(5)
+getgenv().fishItConfig = {
+    ADD_FRIEND = true,
+    AUTO_UPDATE_RESTART = true,
+    PERFECT_CASTING = true,
+    FISHING_LOCATION = { 
+        ["Fisherman Island"] = { "Starter Rod", "Luck Rod" },
+        ["Tropical Grove"] = { "Lava Rod", "Lucky Rod", "Steampunk Rod", "Astral Rod", "Hazmat Rod", "Ghostfinn Rod", "Element Rod", "Diamond Rod" },
     },
 
-    SellInterval = 300 -- (second)
+    AUTO_QUEST = {
+        -- Coin Quest
+        ["Lost Treasure"] = true,
+        ["Steve's Lost Coins"] = true,
+
+        -- Rod Quest
+        ["Deep Sea Quest"] = true,  -- Start when have Astral or Better Rod
+        ["Element Quest"] = true,  -- Start when have Ghostfinn Rod
+        ["Diamond Researcher"] = true,  -- Start when have Element Rod
+
+        -- Potion Quest
+        ["Steve's Lost Keys"] = true,
+    },
+
+    KEEP_FISH_ODDS = 250000,
+    KEEP_FISH_NAME = { "Ruby" },
+    BUY_FISHING_ROD = { "Lucky Rod", "Steampunk Rod", "Astral Rod" },
+    BUY_BAIT = { "Midnight Bait", "Chroma Bait", "Corrupt Bait", "Aether Bait" },
+    BUY_TRAVELING_MERCHANT = { "Singularity Bait", "Hazmat Rod" },
+    CONSUME_POTION = { "Luck I Potion", "Luck II Potion", "Mutation I Potion" },
+    ENCHANT_ROD = { 
+        ["Steampunk Rod"] = { "Mutation Hunter I", "Mutation Hunter II", "Gold Digger I", "Glistening I", "Leprechaun I", "Leprechaun II", "Stormhunter I", "Stargazer I", "Empowered I", "XPerienced I", "Prismatic I", "Reeler I", "Big Hunter I" },
+        ["Astral Rod"] = { "Leprechaun II" },
+        ["Ghostfinn Rod"] = { "Leprechaun II" },
+        ["Element Rod"] = { "Leprechaun II" },
+    },
+
+    -- Auto Gift
+    AUTO_GIFT_USER = {},  -- Username to gift
+    GIFT_FISH_ODDS = 250000,
+    GIFT_ITEMS = { "Transcended Stone" },  -- Item Names
+
+    -- Webhook
+    WEBHOOK_FISH_ODDS = 250000,
+    WEBHOOK_URL = "",
+    DISCORD_ID = "",
+    WEBHOOK_NOTE = "",
+    SHOW_PUBLIC_DISCORD_ID = true,
+    SHOW_WEBHOOK_USERNAME = true,
+    SHOW_WEBHOOK_JOBID = false,
 }
-getgenv().Key = "MARU-TN4X8-SJSMZ-4WH3-DJ4GH-SA7K"
-getgenv().id = "935143471243726858"
-getgenv().Script_Mode = "Kaitun_Script"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/db8c893d7f0773653a850f508218b539.lua"))()
