bfb927e552055d510e8dd1a0e71b0eaf82520424
Duyy <anhhoangduy659@gmail.com>
2026-01-23 22:09:00 +0700

    Update mainscript.lua

--- FIsch/mainscript.lua
+++ FIsch/mainscript.lua
@@ -1,25 +1,26 @@
 _G.config = {
-    fishing_position = CFrame.new(3726.26172, -1127.95703, -1070.00867, 0.732412577, 2.20410072e-08, 0.680861115, 8.9077199e-09, 1, -4.19544186e-08, -0.680861115, 3.67928621e-08, 0.732412577),
+    build = "0.0a",
+    fishing_position = CFrame.new(3702.94043, -1102.62109, -1089.09399, 0.0015249321, 0.00411154795, -0.999990404, -0.00142530177, 0.999990523, 0.00410937518, 0.999997795, 0.00141902152, 0.0015307779),
     rod = {
         free_gift = "Fabulous Rod",
         main = {
             "Original No-Life Rod",
             "Carrot Rod",
-            "Luminescent Oath",
+            -- "Luminescent Oath",
             "Great Rod of Oscar",
         },
         second = {
-            "Ethereal Prism Rod", "Cerulean Fang Rod", "Aurora Rod", "Tempest Rod",
-            "Abyssal Specter Rod", "Destiny Rod", "Challenger's Rod", "Rod Of The Zenith",
+            "Ethereal Prism Rod", "Cerulean Fang Rod", "Aurora Rod", "Tempest Rod", "Anchor n' Chain",
+            "Abyssal Specter Rod", "Destiny Rod", "Challenger's Rod", "Rod Of The Zenith", "Tidemourner",
             "Nocturnal Rod", "Kings Rod", "Trident Rod", "Poseidon Rod", "Champions Rod",
-            "Volcanic Rod", "Summit Rod", "Training Rod", "Fast Rod", "Steady Rod",
-            "Precision Rod", "Depthseeker Rod", "Vocanic Rod", "Voyager Rod",
+            "Volcanic Rod", "Summit Rod", "Training Rod", "Fast Rod", "Steady Rod", "Thalassar's Ruin",
+            "Precision Rod", "Depthseeker Rod", "Vocanic Rod", "Treasure Rod", 'Fallen Rod',
             "Riptide Rod", "Rainbow Cluster Rod", "The Boom Ball", "Scarlet Spincaster Rod",
             "Zeus Rod", "Kraken Rod", "Leviathan's Fang Rod", "Celestial Rod", "Rod Of The Depths",
             "Rod Of The Forgotten Fang", "Plastic Rod", "Carbon Rod", "Long Rod", "Lucky Rod",
             "Fortune Rod", "Rapid Rod", "Magnet Rod", "Mythical Rod", "Midas Rod", "Scurvy Rod",
             "Phoenix Rod", "Arctic Rod", "Crystalized Rod", "Ice Warpers Rod", "Avalanche Rod",
-            "Heaven's Rod", "Magma Rod", "Firefly Rod", "Frog Rod", "Frostbane Rod", "Tryhard Rod",
+            "Heaven's Rod", "Magma Rod", "Firefly Rod", "Frog Rod", "Frostbane Rod", "Blazebringer Rod",
             "Azure Of Lagoon", "Free Spirit Rod", "Verdant Shear Rod", "Stone Rod","Wildflower Rod", --"Carrot Rod",
             "Great Dreamer Rod", "Wicked Fang Rod", "Fungal Rod", "Duskwire", "Sanguine Spire", "Boreal Rod",
             "Cryolash"
@@ -62,14 +63,14 @@ _G.config = {
         rod = {
             ["Great Rod of Oscar"] = {
                 ["Enchant Relic"] = {
-                    LevelFarm = {"Clever"},
-                    CashFarm = {"Clever"},
+                    levelfarm = {"Clever"},
+                    cashfarm = {"Clever"},
                 },
             },
             ["Luminescent Oath"] = {
                 ["Enchant Relic"] = {
-                    LevelFarm = {"Clever", "Insight", "Sea King"},
-                    CashFarm = {"Sea King"},
+                    levelfarm = {"Clever", "Insight", "Sea King"},
+                    cashfarm = {"Sea King"},
                 },
             },
         },
@@ -90,6 +91,7 @@ _G.config = {
     },
     horst_manager = {
         log_style = "Default", -- Default, Brackets, Horizontal
+        auto_send_log = true,
         auto_change_account = true,
         changed_on = {
             level = {
@@ -97,11 +99,11 @@ _G.config = {
                 amount = 2000,
             },
             rod = {
-                enable = true,
+                enable = false,
                 amount = 50,
             },
             cash = {
-                enable = true,
+                enable = false,
                 amount = 10000000,
             },
         },
