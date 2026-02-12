84da1b4b14cf9c24a69fe04087dd7a8796281c18
Duyy <anhhoangduy659@gmail.com>
2026-02-12 22:33:33 +0700

    a

--- Anime Vanguard - Tea Farm/Mainscript.lua
+++ Anime Vanguard - Tea Farm/Mainscript.lua
@@ -1,9 +1,14 @@
 if not game:IsLoaded() then
     repeat game.Loaded:Wait() until game:IsLoaded()
 end
-if game.PlaceId ~= 18219125606 then
-    game:GetService('TeleportService'):Teleport(18219125606)
-end
-if game.PlaceId == 18219125606 then
-    setfpscap(2)
+wait(30)
+while(true)
+do
+    if game.PlaceId ~= 18219125606 then
+        game:GetService('TeleportService'):Teleport(18219125606)
+    end
+    if game.PlaceId == 18219125606 then
+        setfpscap(2)
+    end
+    wait(60)
 end
